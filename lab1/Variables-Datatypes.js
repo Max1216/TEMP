0ba754885feab00ed3354dac9cc14aa1d64f90c3
Max1216 <max.ruotsalainen@abo.fi>
2025-12-02 18:23:25 +0200

    Lade till allting egna anteckningar från lab1

---
+++ lab1/Variables-Datatypes.js
@@ -0,0 +1,12 @@
+var nameJ = "John";
+console.log("nameJ", nameJ);
+nameJ = "Jane";
+console.log("nameJ", nameJ);
+
+let age = 25;
+console.log("age", age);
+age = 26;
+console.log("age", age);
+
+const PI = 3.14159;
+console.log("PI", PI);
